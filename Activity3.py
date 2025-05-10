139f6feaa095a06bfd07c091d146276986655978
Mahbeer2012 <mahbeermubashir031@gmail.com>
2025-05-10 15:52:32 +0500

    Activity3.py

--- Activity3.py
+++ Activity3.py
@@ -1,17 +1,17 @@
 import random
 
 class FruitQuiz:
-    # Create a constructor
-    def __init__(self):
+# Create a constructor
+  def __init__(self):
 
-        # Create a dictionary of fruits as keys and color as value
+    # Create a dictionary of fruits as keys and color as value
         self.fruits = {'apple':'red',
                        'orange':'orange',
                        'watermelon':'green',
                        'banana':'yellow'}
 
 # function for the quiz, here a fruit will be chosen randomly
-def quiz(self):
+  def quiz(self):
     while(True):
 
         fruit, colour = random.choice(list(self.fruits.items()))
@@ -27,7 +27,6 @@ def quiz(self):
         option = int(input("enter 0 , if you want to play again otherwise enter 1:"))
         if (option):
             break
-        print("welcome to fruit quiz")
-        fq = FruitQuiz()
-        fq.quiz()
-              
+print("welcome to fruit quiz")
+fq = FruitQuiz()
+fq.quiz()
